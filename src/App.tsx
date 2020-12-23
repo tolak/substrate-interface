b50829299af7a67e7b90084f8e9ab9f4bd5431a6
Wenfeng Wang <kalot.wang@gmail.com>
2020-12-23 11:47:14 +0800

    Use React.FC template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import logo from './logo.gif';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false)
+
+  const handlePresentMobileMenu = useCallback(() => {
+    // do nothing
+  }, [setMobileMenu])
+
+  const handleDismissMobileMenu = useCallback(() => {
+    // do nothing
+  }, [setMobileMenu])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    <HashRouter>
+    {/* <TopBar onPresentMobileMenu={handlePresentMobileMenu} />
+    <MobileMenu onDismiss={handleDismissMobileMenu} visible={mobileMenu} /> */}
+    <Switch>
+      <Route path="/" exact>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Route>
+      <Route path="/app">
+        <h1>app page</h1>
+      </Route>
+      <Route path="/faq">
+        <h1>faq page</h1>
+      </Route>
+    </Switch>
+    </HashRouter>
+    );
 }
 
 export default App;
