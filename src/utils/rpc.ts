bb2d8a4f9f9e9ad997a97d1a857ffe7e4ae45d40
Wenfeng Wang <kalot.wang@gmail.com>
2020-12-23 11:45:45 +0800

    Use react-scripts to catch environment

--- src/utils/rpc.ts
+++ src/utils/rpc.ts
@@ -1,11 +1,13 @@
-import '../config'
 import { ApiPromise, WsProvider} from '@polkadot/api';
 
 export const connect = async (types: any) => {
-    console.log('Using rpc node ws url ', process.env.CHAIN_NODE_WS_URL)
-    console.log('Using rpc node ws port:', process.env.CHAIN_RPC_WS_PORT)
-	const provider = new WsProvider(process.env.CHAIN_NODE_WS_URL + process.env.CHAIN_RPC_WS_PORT)
-	const api = new ApiPromise({ provider, types })
-	await api.isReady
-	return api
+    console.log('Using rpc node ws url ', process.env.REACT_APP_CHAIN_NODE_WS_URL)
+    console.log('Using rpc node ws port:', process.env.REACT_APP_CHAIN_RPC_WS_PORT)
+    if(process.env.REACT_APP_CHAIN_NODE_WS_URL && process.env.REACT_APP_CHAIN_NODE_WS_URL) {
+        const provider = new WsProvider(process.env.REACT_APP_CHAIN_NODE_WS_URL + ':' + process.env.REACT_APP_CHAIN_RPC_WS_PORT)
+        const api = new ApiPromise({ provider, types })
+        await api.isReady
+        return api
+    }
+    return null
 }
